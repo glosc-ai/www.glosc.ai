fc65c7b48c28560a573cc5694676551565a1c6fc
小莫 <xm@aoe.top>
2025-12-11 18:00:00 +0800

    跑通 MCP 工具调用

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,13 +1,41 @@
-import { convertToModelMessages, streamText, UIMessage } from "ai";
+import {
+    convertToModelMessages,
+    streamText,
+    ToolSet,
+    UIMessage,
+    tool,
+    stepCountIs,
+    jsonSchema,
+} from "ai";
 
 // http://localhost:3000/api/chat
 export default defineEventHandler(async (event) => {
-    const { messages, model }: { messages: UIMessage[]; model: string } =
-        await readBody(event);
+    const {
+        messages,
+        model,
+        tools: clientTools,
+    }: {
+        messages: UIMessage[];
+        model: string;
+        webSearch: boolean;
+        tools?: Record<string, any>;
+    } = await readBody(event);
+
+    const tools: ToolSet = {};
+    if (clientTools) {
+        for (const [name, toolDef] of Object.entries(clientTools)) {
+            tools[name] = tool({
+                description: toolDef.description,
+                parameters: jsonSchema(toolDef.parameters),
+            } as any);
+        }
+    }
 
     const result = streamText({
-        model: model || "xai/grok-4",
+        model: model || "xai/grok-code-fast-1",
         messages: convertToModelMessages(messages),
+        tools,
+        stopWhen: stepCountIs(10),
     });
 
     return result.toUIMessageStreamResponse();
---
+++ server/api/v1/[...path].ts
@@ -0,0 +1,18 @@
+import {
+    eventHandler,
+    proxyRequest,
+    getRouterParam,
+    getProxyRequestHeaders,
+} from "h3";
+
+export default eventHandler(async (event) => {
+    const path = getRouterParam(event, "path") || "";
+    const target = `https://ai-gateway.vercel.sh/v1/ai/${path}`;
+    const AI_GATEWAY_API_KEY = process.env.AI_GATEWAY_API_KEY || "";
+    return proxyRequest(event, target, {
+        headers: {
+            ...getProxyRequestHeaders(event),
+            Authorization: `Bearer ${AI_GATEWAY_API_KEY}`,
+        },
+    });
+});
